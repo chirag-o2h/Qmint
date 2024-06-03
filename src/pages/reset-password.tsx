7cb124fbad7494251173572c79bc07f861d8a242
Ayushi Talaviya <ayushitalaviya@Aayushis-MacBook-Air.local>
2024-06-03 13:02:43 +0530

    Feat:forgot password screen ui done

---
+++ src/pages/reset-password.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function ResetPassword() {
+  return (
+    <>
+      <Typography>test</Typography>
+    </>
+  )
+}
+
+export default ResetPassword
