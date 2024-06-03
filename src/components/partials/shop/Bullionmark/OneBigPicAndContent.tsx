4d9ce947ff9a01d26f017ae242ace0a9ab2b3edd
Ayushi Talaviya <ayushitalaviya@Aayushis-MacBook-Air.local>
2024-06-03 17:51:52 +0530

    feat::shop page gold ganner ui done

--- src/components/partials/shop/Bullionmark/OneBigPicAndContent.tsx
+++ src/components/partials/shop/Bullionmark/OneBigPicAndContent.tsx
@@ -1,19 +1,30 @@
 import { useAppSelector } from '@/hooks'
-import { Box, Container } from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
 import React from 'react'
+import goldbannerImage from "@/assets/images/goldbannerImage.png"
 
 function OneBigPicAndContent() {
   const bmkShopSections = useAppSelector(state => state.homePage.bmkShopPageSections)
 
   return (
-    bmkShopSections && <Box id="OneBigPicSlider" component="section">
+    bmkShopSections && <Box id="OneBigPicSlider" component="section"
+      sx={{ backgroundImage: `url(${goldbannerImage})` }}
+    >
       <Box className="ck-content">
-        <Box dangerouslySetInnerHTML={{
+        {/* <Box dangerouslySetInnerHTML={{
           __html: bmkShopSections["shopHomepage_Section_4_One_big_pic_and_content"]
-        }}>
+        }}> */}
+        <Box className="BannerWrapper">
+          <Typography variant="body2" className="OfferTag">50% OFF ON</Typography>
+          <Typography variant="h3" className="BannerTitle">Bullionmark Kangaroo
+            Gold Cast Bar</Typography>
+          <Typography variant="body2" className="BannerDescription">See our Shipping out off dates</Typography>
+          <Button variant="contained">Shop Now</Button>
         </Box>
+        {/* </Box> */}
       </Box>
-    </Box>)
+    </Box >
+  )
 }
 
 export default OneBigPicAndContent
