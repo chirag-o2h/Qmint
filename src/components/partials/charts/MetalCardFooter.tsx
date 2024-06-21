a1717c2b413d63ffcfde578cdaebb2dbc2ca5925
Ayushi Talaviya <ayushitalaviya@Aayushis-MacBook-Air.local>
2024-06-21 16:53:09 +0530

    feat:fix layout

--- src/components/partials/charts/MetalCardFooter.tsx
+++ src/components/partials/charts/MetalCardFooter.tsx
@@ -7,8 +7,10 @@ function MetalCardFooter() {
     /* pass UpTrend, DownTrend, Neutral,NaN  class */
     <Stack className='MetalCardFooter UpTrend'>
       <Typography variant="subtitle1" sx={{ textTransform: "uppercase" }}>UP TREND</Typography>
-      <Typography variant="body1"><FilledUpButton />15.12</Typography>
-      <Typography variant="body1">0.44%</Typography>
+      {/* <Typography variant="body1"><FilledUpButton />15.12</Typography> */}
+      {/* <Typography variant="body1">0.44%</Typography> */}
+
+      {/* <Typography variant="subtitle1" sx={{ textTransform: "uppercase" }}><FilledUpButton />INCREASING</Typography> */}
     </Stack>
   )
 }
