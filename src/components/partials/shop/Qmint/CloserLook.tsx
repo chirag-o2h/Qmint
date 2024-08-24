a9d0c7710ac2f911fa34fbc13c59e3cab23724b4
chirag <chirag.gohil@02h.com>
2024-08-24 14:04:14 +0530

    fix: both side slider and swipe issue is solved

--- src/components/partials/shop/Qmint/CloserLook.tsx
+++ src/components/partials/shop/Qmint/CloserLook.tsx
@@ -109,60 +109,60 @@ function CloserLook({ configDetails, homePageSectionDetails }: { configDetails: 
 
   return (
     homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.length !== 0 ?
-    <Box id="CloserLook">
-      <Container component="section">
-        <SectionHeading
-          title={configDetails?.["ShopHomepage_Section_6_Three_posts_in_a_row_Title"]?.value ?? "Take a closer look*"}
-          description={configDetails?.["ShopHomepage_Section_6_Three_posts_in_a_row_Subtitle"]?.value ?? "description*"}
-        />
-        <Container className="DestinationWrapper" maxWidth="lg">
-          {homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.length !== 0 ?
-            <Box className="SwiperContainer">
-              <Swiper {...config} >
-                {
-                  !loading ?
-                    (homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.length > 0 ? homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.map((destination: { id: React.Key | null | undefined; friendlyName: any; title: any; overview: any; imageUrl: any }) => (
-                      <SwiperSlide key={destination.id}>
-                        <TravelCard
-                          friendlyName={destination?.friendlyName}
-                          place={destination.title}
-                          description={destination.overview}
-                          imageUrl={destination.imageUrl}
-                        />
-                      </SwiperSlide>
-                    ))
-                      : null) :
-                    Array(5).fill(0).map((_, index) => {
-                      return (
-                        <SwiperSlide key={index}>
-                          <Card className="ProductCard">
-                            <Skeleton animation="wave" height={500} style={{ borderRadius: "10px 10px 0 0", padding: "0px" }} />
-                            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-                              <Skeleton animation="wave" height={95} width="95%" style={{ marginBottom: "4px" }} />
-                              <Skeleton animation="wave" height={70} width="95%" />
-                            </div>
-                          </Card>
+      <Box id="CloserLook">
+        <Container component="section">
+          <SectionHeading
+            title={configDetails?.["ShopHomepage_Section_6_Three_posts_in_a_row_Title"]?.value ?? "Take a closer look*"}
+            description={configDetails?.["ShopHomepage_Section_6_Three_posts_in_a_row_Subtitle"]?.value ?? "description*"}
+          />
+          <Container className="DestinationWrapper" maxWidth="lg">
+            {homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.length !== 0 ?
+              <Box className="SwiperContainer">
+                <Swiper {...config} >
+                  {
+                    !loading ?
+                      (homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row?.length > 0 ? [...homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row, ...homePageSectionDetails?.shopHomepage_Section_6_Three_posts_in_a_row]?.map((destination: { id: React.Key | null | undefined; friendlyName: any; title: any; overview: any; imageUrl: any }) => (
+                        <SwiperSlide key={destination.id}>
+                          <TravelCard
+                            friendlyName={destination?.friendlyName}
+                            place={destination.title}
+                            description={destination.overview}
+                            imageUrl={destination.imageUrl}
+                          />
                         </SwiperSlide>
-                      );
-                    })
-                }
-              </Swiper>
-            </Box>
-            : <RecordNotFound message="No destination available" />
-          }
-        </Container>
-        {/* <Stack className="Action" onClick={() => {
+                      ))
+                        : null) :
+                      Array(5).fill(0).map((_, index) => {
+                        return (
+                          <SwiperSlide key={index}>
+                            <Card className="ProductCard">
+                              <Skeleton animation="wave" height={500} style={{ borderRadius: "10px 10px 0 0", padding: "0px" }} />
+                              <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                                <Skeleton animation="wave" height={95} width="95%" style={{ marginBottom: "4px" }} />
+                                <Skeleton animation="wave" height={70} width="95%" />
+                              </div>
+                            </Card>
+                          </SwiperSlide>
+                        );
+                      })
+                  }
+                </Swiper>
+              </Box>
+              : <RecordNotFound message="No destination available" />
+            }
+          </Container>
+          {/* <Stack className="Action" onClick={() => {
           navigate('/blog')
         }}>
         <Button aria-label={'DiscoverMore'} name={'DiscoverMore'} variant="contained">Discover More</Button>
       </Stack> */}
-        <Stack className="Action">
-          <Button className="DiscoverMore" name='CloserLook' aria-label="CloserLook" variant="contained" onClick={() => {
-            navigate('/blog')
-          }}>Discover More</Button>
-        </Stack>
-      </Container>
-    </Box>:null
+          <Stack className="Action">
+            <Button className="DiscoverMore" name='CloserLook' aria-label="CloserLook" variant="contained" onClick={() => {
+              navigate('/blog')
+            }}>Discover More</Button>
+          </Stack>
+        </Container>
+      </Box> : null
   )
 }
 
