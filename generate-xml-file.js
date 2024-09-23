3daecbac1bcfc223eb14457fdd9c4298f5e17fa2
chirag <chirag.gohil@02h.com>
2024-09-23 17:24:44 +0530

    fix: remove the extra slash

--- generate-xml-file.js
+++ generate-xml-file.js
@@ -32,7 +32,7 @@ const generateXml = async () => {
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
   urls.forEach(url => {
-    xml += `  <url>\n    <loc>${baseUrl}/${url.linkUrl}</loc>\n    <changefreq>daily</changefreq>\n    <priority>0.7</priority>\n  </url>\n`;
+    xml += `  <url>\n    <loc>${baseUrl}${url.linkUrl}</loc>\n    <changefreq>daily</changefreq>\n    <priority>0.7</priority>\n  </url>\n`;
   });
 
   xml += `</urlset>`;
